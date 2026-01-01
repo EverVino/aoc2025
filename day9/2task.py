637934237879a8f62e37f8e82d846501d9e5affb
EverVino <evervino00@gmail.com>
2026-01-01 19:38:42 -0400

    update day9 p2

--- day9/2task.py
+++ day9/2task.py
@@ -45,21 +45,127 @@ def get_borders(data):
 
     for k, v in pairs.items():
         if len(v) > 2:
-            print(k, v)
             x, y=k 
+            nl = []
+            ol = []
             for p in v:
-                xi,yi = p
-                if yi!=y:
-                    print(p, pairs[p])
-                
+                xi, yi = p
+                if yi != y:
+                    nl.append(p)
+                else:
+                    ol.append(p)
+            nol = sorted(ol, key= lambda m:abs(m[0]-x))
+            nl.append(nol[0])
+            pairs[k] = nl
+        
         if len(v)<2:
             print(k,v)
+    return pairs
 
 data = open_data()
-rows, cols = get_dim(data)
+#rows, cols = get_dim(data)
 #matrix = [[0 for _ in range(cols)] for __ in range(rows)]
 #for point in data:
 #    x, y = point
 #    matrix[y][x] = 1
 
-get_borders(data)
+pairs = get_borders(data)
+borders = [] 
+for k, v in pairs.items():
+    for p in v:
+        border = sorted((k,p))
+        if border not in borders:
+            borders.append(border)
+print(len(borders))
+print(len(pairs))
+
+def get_vs(p1,p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    if x1>x2:
+        xM = x1
+        xm = x2
+    else:
+        xM = x2
+        xm = x1
+    if y1>y2:
+        yM = y1
+        ym = y2
+    else:
+        yM = y2
+        ym = y1
+    return xm, xM, ym, yM
+
+def intersection(xm, xM, ym, yM, borders):
+    for border in borders:
+        # check if border is vertical
+        p1, p2 = border
+        x1, y1 = p1
+        x2, y2 = p2
+        ys = sorted((y1,y2))
+        xs = sorted((x1,x2))
+
+        if x1 == x2:
+            if xm<x1<xM:
+                if ys[0]<ym<ys[1]:
+                    return True
+                if ys[0]<yM<ys[1]:
+                    return True
+        if y1==y2:
+            if ym<y1<yM:
+                if xs[0]<xm<xs[1]:
+                    return True
+                if xs[0]<xM<xs[1]:
+                    return True
+    return False
+
+
+
+n = len(data)
+A = 0
+for i in range(n):
+    for j in range(i+1,n):
+        p1 = data[i]
+        p2 = data[j]
+    xm, xM, ym, yM = get_vs(p1,p2)
+    if not intersection(xm, xM, ym, yM, borders):
+        a = (abs(p1[0]-p2[0])+1)*(abs(p1[1]-p2[1])+1)
+        print(a)
+        if abs(a) > A:
+            A = abs(a)
+
+print(A)
+# check borders
+#n = len(borders)
+#counter = {}
+#for k in pairs.keys():
+#    for j in range(n):
+#        p1, p2 = borders[j]
+#        if p1 == k or p2 == k:
+#            if k not in counter:
+#                counter[k] = 0
+#            counter[k] += 1
+#
+#start = borders[0][0]
+#init_ = borders[0][0]
+#next_ = borders[0][1]
+#c=0
+#while next_ != init_:
+#    for pair in borders:
+#        p1, p2 = pair
+#        if p1 == next_ and p2 != start:
+#            start = next_
+#            next_ =p2
+#
+#            break
+#        
+#        if p2 ==next_ and p1 != start:
+#            start = next_
+#            next_ = p1
+#            break
+#    c += 1
+#    print(c)
+#print(c)
+    
+
+
