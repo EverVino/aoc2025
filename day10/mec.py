f9e487e1f7d19db83160e5bc5db910c6898a9854
EverVino <evervino00@gmail.com>
2026-01-25 00:05:24 -0400

    finally day10

--- day10/mec.py
+++ day10/mec.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.linalg import qr
+
 import sympy as sp
 
 def open_data(path='input.txt'):
@@ -69,7 +71,7 @@ def solve_with_parameters(variables, expanded_matrix):
         B = np.array(features[:,: -equations], dtype=int)
         A = np.array(features[:, -equations :], dtype=int)
     max_value = C.max()
-
+    min_ans = None
     min_sum = float("inf")
     try:
         A_inv = np.linalg.inv(A)
@@ -78,27 +80,29 @@ def solve_with_parameters(variables, expanded_matrix):
         print(expanded_matrix)
         print(e)
         return 0
-    for value in set_values(parameters, max_value):
+    for value in set_values(parameters, int(max_value)):
         if value.sum() > min_sum:
             continue
         M = C - B@value
         res = A_inv@M
             #res = np.linalg.solve(A.astype("float64"), M.astype("float64"))
-
-        res[np.abs(res)< 1e-8]=0
-
+        #print(f"ans for value {value}")
+        #print(res)
+        res[np.abs(res)< 1e-8] = 0
         if (res < 0).any():
             continue
 
-        res_int = np.round(res.astype(np.float64), decimals=2).astype(int)
-        valid = abs((res.astype(np.float64) - res_int).sum()) < 1e-8
+        res_int=np.round(res, decimals=2).astype(int)
+        valid = abs((res_int - res).sum()) < 1e-5
 
         if not valid:
             continue
+
         partial_sum = np.sum(res) + value.sum() 
         if partial_sum < min_sum:
             min_sum = partial_sum
-
+            min_ans = res.copy()
+    #print(min_ans)
     return min_sum
 
 def get_min_value(light, wirings):
@@ -115,25 +119,46 @@ def get_min_value(light, wirings):
 
     Mf = Mnp[mask,:]
 
-    return solve_with_parameters(variables, Mf)
+    return solve_with_parameters(variables, Mf.astype(np.float64))
+
+def solve_specials(light, wirings):
+    n = light.shape[0]
+    variables = wirings.shape[0]
+    A = wirings.T
+    C = np.atleast_2d(light).T
+    #print('matrix A')
+    #print(A)
+    #print('matrix C')
+    #print(C)
+    Q, R, piv = qr(A, pivoting=True)
+
+    rank = np.linalg.matrix_rank(A)
+    independent_cols = piv[:rank]
+    dependant_cols = piv[rank:]
+    #print('idx')
+    #print(independent_cols)
+    #print(dependant_cols)
+    valid_A = A[:, independent_cols]
+    valid_B = A[:, dependant_cols]
+    Mi = np.append(valid_A, valid_B, axis=1)
+
+    M = np.append(Mi, C, axis=1)
+    #print('M')
+    #print(M)
+    return solve_with_parameters(variables, M)
 
 data = open_data()
-#acc = 0
-#machine = data[1]
-#light = machine['joltage']
-#wirings = machine['wiring']
-#value = get_min_value(light, wirings)
-#print(value)
 
 acc = 0
 no_process = [7, 41, 47, 73, 102]
 no_sum = [20, 30, 92, 105, 114]
 for i, machine in enumerate(data):
     if i in no_process + no_sum:
-        continue
-    light = machine['joltage']
-    wirings = machine['wiring']
-    min_value = get_min_value(light, wirings)
-    print(i, min_value)
-    acc += min_value
+        light = machine['joltage']
+        wirings = machine['wiring']
+        min_value = solve_specials(light, wirings)
+        #min_value = get_min_value(light, wirings)
+        print(i, min_value)
+        acc += min_value if min_value != float('inf') else 0
+
 print(acc)
